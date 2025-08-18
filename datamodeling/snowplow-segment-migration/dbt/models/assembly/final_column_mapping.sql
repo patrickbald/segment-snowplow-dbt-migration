55ae4795d46e10ee99d8f1ebb0701aa759381f64
patrickbald <pbaldvt@gmail.com>
2025-08-18 14:24:06 -0400

    update to event renaming

--- datamodeling/snowplow-segment-migration/dbt/models/assembly/final_column_mapping.sql
+++ datamodeling/snowplow-segment-migration/dbt/models/assembly/final_column_mapping.sql
@@ -1,7 +1,7 @@
-{#- Get the event mapping dictionary from the project vars -#}
-{%- set event_map = var('event_rename_map', {}) -%}
-{#- Get the context definitions from the project vars -#}
+{#- Get the event mapping dictionaries from the project vars -#}
+{%- set source_relation_event_map = var('event_rename_map', {}) -%}
 {%- set context_definitions = var('context_definitions', {}) -%}
+
 
 WITH unioned_events AS (
     SELECT * FROM {{ ref('base_column_mapping') }}
@@ -9,45 +9,46 @@ WITH unioned_events AS (
 
 SELECT
     -- 1. Select the final, correctly-named Snowplow atomic event columns directly
-    event_id
-    ,userid
-    ,domain_userid
-    ,collector_tstamp
-    ,derived_tstamp
-    ,dvce_created_tstamp
-    ,dvce_sent_tstamp
-    ,user_ipaddress
-    ,useragent
-    ,page_url
-    ,page_title
-    ,page_referrer
-    ,page_urlpath
-    ,os_timezone
-    ,br_lang
-    ,br_cookies
-    ,dvce_screenwidth
-    ,dvce_screenheight
-    ,event_vendor
-    ,name_tracker
-    ,v_tracker
+    EVENT_ID
+    ,USERID
+    ,DOMAIN_USERID
+    ,COLLECTOR_TSTAMP
+    ,DERIVED_TSTAMP
+    ,DVCE_CREATED_TSTAMP
+    ,DVCE_SENT_TSTAMP
+    ,USER_IPADDRESS
+    ,USERAGENT
+    ,PAGE_URL
+    ,PAGE_TITLE
+    ,PAGE_REFERRER
+    ,PAGE_URLPATH
+    ,OS_TIMEZONE
+    ,BR_LANG
+    ,BR_COOKIES
+    ,DVCE_SCREENWIDTH
+    ,DVCE_SCREENHEIGHT
+    ,EVENT_VENDOR
+    ,NAME_TRACKER
+    ,V_TRACKER
     
-    -- 2. Keep the original event name for reference, and create the normalized name
-    ,event AS original_event_name
+    -- 2. Keep the original event name for reference, and create the final event name
+    ,EVENT AS original_event_name
     ,CASE
-        {% for snowplow_event, segment_events in event_map.items() %}
-        WHEN event IN (
-            {%- for event_name in segment_events -%}
-            '{{ event_name }}'
-            {{- ',' if not loop.last }}
-            {%- endfor -%}
-        ) THEN '{{ snowplow_event }}'
+        {% for snowplow_event, relation_patterns in source_relation_event_map.items() %}
+        WHEN (
+            {% for pattern in relation_patterns -%}
+            LOWER(SOURCE_RELATION) LIKE LOWER('{{ pattern }}')
+            {%- if not loop.last %} OR {% endif -%}
+            {% endfor %}
+        )
+        THEN '{{ snowplow_event }}'
         {% endfor %}
-        ELSE event
-    END AS event
+        ELSE EVENT
+    END as event
 
     -- 3. Dynamically create a separate column for each context
     {% for context_var_name, context_map in context_definitions.items() %}
-    {%- set data_column = context_var_name ~ '_data' -%}
+    {%- set data_column = (context_var_name ~ '_data') | upper -%}
     ,ARRAY_CONSTRUCT_COMPACT(
         IFF(
             {{ data_column }} IS NOT NULL AND {{ data_column }} != PARSE_JSON('{}'),
@@ -61,6 +62,6 @@ SELECT
     {% endfor %}
 
     -- 4. Pass through any other columns for final analysis or auditing
-    ,source_relation
+    ,SOURCE_RELATION
 
 FROM unioned_events
