b9f8a0f2e430e52a7ccb882e398a6082d0048fd4
patrickbald <pbaldvt@gmail.com>
2025-09-04 11:10:29 -0500

    fix resource context

--- datamodeling/snowplow-segment-migration/dbt/macros/apply_custom_contexts.sql
+++ datamodeling/snowplow-segment-migration/dbt/macros/apply_custom_contexts.sql
@@ -1,11 +1,28 @@
--- macros/apply_custom_contexts.sql
-
 {% macro apply_custom_contexts() %}
+    {%- set custom_context_definitions = var('custom_context_definitions', {}) -%}
     {%- set custom_context_mappings = var('custom_context_mappings', {}) -%}
     {%- set custom_context_schemas = var('custom_context_schemas', {}) -%}
     
-    {#- Generate each custom context column -#}
+    {#- First, handle column-based custom contexts from definitions -#}
+    {%- for context_var_name, context_map in custom_context_definitions.items() -%}
+    {%- set data_column = (context_var_name ~ '_data') | upper -%}
+    {%- set schema_url = custom_context_schemas.get(context_var_name, '') -%}
+    ,ARRAY_CONSTRUCT_COMPACT(
+        IFF(
+            {{ data_column }} IS NOT NULL AND {{ data_column }} != PARSE_JSON('{}'),
+            OBJECT_CONSTRUCT(
+                'schema', '{{ schema_url }}',
+                'data', {{ data_column }}
+            ),
+            NULL
+        )
+    ) AS {{ ('contexts_com_desiringgod_' ~ context_var_name ~ '_1') | upper }}
+    {%- endfor -%}
+    
+    {#- Then handle event-based custom contexts from mappings -#}
     {%- for context_name, schema_url in custom_context_schemas.items() -%}
+    {#- IMPORTANT: Skip contexts already handled by definitions -#}
+    {%- if context_name not in custom_context_definitions -%}
     {%- set target_context_type = context_name ~ '_context' -%}
     
     {#- Collect all WHEN clauses for this context -#}
@@ -31,6 +48,7 @@
         NULL
         {%- endif %}
     ) AS {{ ('contexts_com_desiringgod_' ~ context_name ~ '_1') | upper }}
+    {%- endif -%}  {#- End of skip check -#}
     {%- endfor -%}
 
 {% endmacro %}
